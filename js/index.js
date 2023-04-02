4416f07badd6259f5846aa618a4ed7a8c161d260
Manuel Javier <manueljavierfc02032003@gmail.com>
2023-04-02 07:02:19 -0600

    version avanzada de estructura principal js

--- js/index.js
+++ js/index.js
@@ -1,83 +1,128 @@
-// Simulador de ahorro e inversion
+const article1 = document.querySelector('#catalogo1')
+const section1 = document.querySelector('#section1')
+const carritobutton = document.querySelector('#ircarrito')
+const formulario = document.querySelector('#iniciosesion')
+const inputnickname = document.querySelector('#nickname')
+const inputcorreo = document.querySelector('#correo')
 
-class Instrumento{
-    constructor(id,nombre,tasaInteres,gat){
+formulario.onsubmit = (enviar) => {
+    enviar.preventDefault()
+    const infousuario = {
+        nickname: inputnickname.value,
+        correo: inputcorreo.value
+    }
+    localStorage.setItem('infousuario', JSON.stringify(infousuario))
+    formulario.remove()
+    alert('registro exitoso')
+}
+const infousuario = localStorage.getItem('infousuario')
+const infousuarioJS = JSON.parse(infousuario)
+console.log(infousuarioJS)
+
+class Juego{
+    constructor(id,nombre,precio,descuento,puntuacion,imagen){
         this.id = id
         this.nombre = nombre
-        this.tasaInteres = tasaInteres
-        this.gat = gat
+        this.precio = precio
+        this.descuento = descuento
+        this.puntuacion = puntuacion
+        this.imagen = imagen
     }
 }
 
-const cetesdirecto = new Instrumento(1,'cetesdirecto',.1104,.1162)
-const compartamos_banco = new Instrumento(2,'compartamos_banco',.0960,.1003)
-const bim = new Instrumento(3,'bim',.0895,.0933)
-const ciBanco = new Instrumento(4,'ciBanco',.0755,.0782)
-const banco_multiva = new Instrumento(5,'banco_multiva',.0725,.0750)
-const banca_mifel = new Instrumento(6,'banca_mifel',.0690,.0712)
-const abcCapital = new Instrumento(7,'abcCapital',.0545,.0559)
-const banamex = new Instrumento(8,'banamex',.0502,.0514)
-const banorte = new Instrumento(9,'banorte',.0386,.0393)
-const bancomer = new Instrumento(10,'bancomer',.0225,.0227)
+const Juegos = [
+    new Juego(1,'dredge',300,.25,4,'./media/imageicon.png'),
+    new Juego(2,'resident evil 4',1200,0,5,'./media/imageicon.png'),
+    new Juego(3,'sons of the fores',350,.2,5,'./media/imageicon.png'),
+    new Juego(4,'the forest',200,.3,5,'./media/imageicon.png'),
+    new Juego(5,'the wild eight',300,.45,4,'./media/imageicon.png'),
+    new Juego(6,'cs:go',0,0,4,'./media/imageicon.png'),
+    new Juego(7,'dayz',600,.2,3,'./media/imageicon.png'),
+    new Juego(8,'forza horizon 4',700,.2,4,'./media/imageicon.png'),
+    new Juego(9,'the long dark',400,.75,3,'./media/imageicon.png'),
+    new Juego(10,'stardew valley',250,.45,5,'./media/imageicon.png'),
+    new Juego(11,'dont starve together',100,.5,4,'./media/imageicon.png'),
+    new Juego(12,'the last of us 1',1000,0,5,'./media/imageicon.png'),
+    new Juego(13,'the last of us 2',1400,0,1,'./media/imageicon.png'),
+    new Juego(14,'elden ring',800,.3,5,'./media/imageicon.png'),
+    new Juego(15,'cult of the lamb',250,.1,5,'./media/imageicon.png'),
+    new Juego(16,'overwatch',0,0,3,'./media/imageicon.png'),
+    new Juego(17,'haven',150,.4,5,'./media/imageicon.png'),
+    new Juego(18,'celeste',100,.6,5,'./media/imageicon.png')
+]
 
-const Instrumentos = [cetesdirecto, compartamos_banco, bim, ciBanco, banco_multiva, banca_mifel, abcCapital, banamex, banorte, bancomer]
+const carrito = []
 
-let infoInstrumentoElegido
-let seleccionado = false
+Juegos.forEach(game => {
+    article1.innerHTML = article1.innerHTML + `<div class="card" style="width: 18rem;">
+    <img src="${game.imagen}" class="card-img-top" alt="icono de prueba">
+    <div class="card-body">
+      <h5 class="card-title">${game.nombre}</h5>
+      <p class="card-text">Precio: ${game.precio} MNX <br>Descuento:${game.descuento*100}% <br>Preio actual:${game.precio-(game.precio*game.descuento)} MNX <br>Calificacion: ${game.puntuacion}</p>
+      <button id=${game.id} class="btn btn-primary">Agregar Al Carrito</button>
+    </div>
+  </div>`
+});
+console.log(Juegos)
+const addcar = document.querySelectorAll('#catalogo1 .card .card-body button')
+console.log(addcar)
 
-let instrumentoelegido = parseInt(prompt('Escoje el numero del Instrumento Financiero con el que deseas cotizar: \n 1.cetesdirecto \n 2.compartamos_banco  \n 3.bim  \n 4.ciBanco  \n 5.banco_multiva  \n 6.banca_mifel  \n 7.abcCapital  \n 8.banamex  \n 9.banorte  \n 10.bancomer'))
+addcar.forEach(btn => {
+    btn.onclick = () =>{
+        const juego = Juegos.find((game) => game.id === parseInt(btn.id))
 
-while(seleccionado === false){
+        const Juegoelegido = {
+            id: juego.id,
+            nombre: juego.nombre,
+            precio: juego.precio,
+            descuento: juego.descuento,
+            puntuacion: juego.puntuacion,
+            imagen: juego.imagen,
+            cantidad : 1
+        }
 
-    infoInstrumentoElegido = Instrumentos.find((infoInstrumentoElegido => infoInstrumentoElegido.id === instrumentoelegido))
-
-    if(!infoInstrumentoElegido){
-        instrumentoelegido = parseInt(prompt('Escoje un numero del Instrumento Financiero valido con el que deseas cotizar: \n 1.cetesdirecto \n 2.compartamos_banco  \n 3.bim  \n 4.ciBanco  \n 5.banco_multiva  \n 6.banca_mifel  \n 7.abcCapital  \n 8.banamex  \n 9.banorte  \n 10.bancomer'))
-    } else{
-        seleccionado = true
+        const Juegoencarrito = carrito.find(g => g.id === Juegoelegido.id)
+        if(Juegoencarrito){
+            Juegoencarrito.cantidad++
+        }else{
+            carrito.push(Juegoelegido)
+        }
+        console.log(carrito)
     }
+});
 
+carritobutton.onclick = () => {
+    article1.remove()
+    carritobutton.remove()
+
+    const artcarrito = document.createElement('article')
+    artcarrito.setAttribute('id', 'artcarrito')
+    section1.append(artcarrito)
+    const tbcarrito = document.createElement('table')
+    tbcarrito.setAttribute('id', 'tbcarrito')
+    tbcarrito.setAttribute('class', 'table')
+    artcarrito.append(tbcarrito)
+    const tbcarh = document.createElement('thead')
+    tbcarrito.append(tbcarh)
+    const tbcarb =document.createElement('tbody')
+    tbcarrito.append(tbcarb)
+
+    tbcarh.innerHTML = `<tr>
+        <th>Juego</th>
+        <th>Precio original</th>
+        <th>Descuento</th>
+        <th>Precio actual</th>
+        <th>Cantidad</th>
+        <th>Total</th>
+    </tr>`
+    carrito.forEach(game => {
+        tbcarb.innerHTML += `<tr>
+            <td>${game.nombre}</td>
+            <td>${game.precio}</td>
+            <td>${game.descuento*100}%</td>
+            <td>${game.precio-(game.precio*game.descuento)}</td>
+            <td>${game.cantidad}</td>
+            <td>${game.cantidad*(game.precio-(game.precio*game.descuento))}</td>
+        </tr>`
+    })
 }
-
-console.log(infoInstrumentoElegido)
-
-let montoInversion = parseFloat(prompt('Ingresa el monto inicial a invertir'))
-let plazoInversion = parseInt(prompt('Ingresa el plazo en años de inversion'))
-let depositos = 0
-let periodicidad = 0
-let da = false
-
-while(da === false){
-
-    let depositoadicional = prompt('desea hacer depositos adicionales? (selecciones "si" o "no")')
-
-    if(depositoadicional == 'si'){
-        depositos = parseFloat(prompt('Ingrese el monto de los depositos adicionales'))
-        periodicidad = parseInt(prompt('Ingrese en dias la periodicidad de los montos adicionales'))
-        da = true
-    }else if (depositoadicional == 'no') {
-        da = true
-    } else {
-        alert('Lo sentimos la opcion marcada no es valida, por favor seleccione una opcion valida')
-    }
-}
-
-
-function calcularInversion(montoInicial, plazo, deposito, periodos, interes) {
-    // convertir los periodos de dias a anos
-    const frecuenciaDepositosAnuales = periodos / 365;
-    // calcular el numero total de depositos
-    const totalDeposits = Math.floor(plazo / frecuenciaDepositosAnuales);
-    // Calcular el valor futuro de la inversion
-    const valorFuturo = montoInicial * Math.pow(1 + interes, plazo);
-    // calcular el valor futuro de los depositos
-    const valorFuturoDepositos = deposito * ((Math.pow(1 + interes, plazo) - 1) / (interes / periodos));
-    // calcular el valor futuro total
-    const valorFuturoTotal = valorFuturo + valorFuturoDepositos;
-  
-    return valorFuturoTotal.toFixed(2); // return total future value rounded to 2 decimal places
-  }
-
-const montoFinalInvercion = calcularInversion(montoInversion,plazoInversion,depositos,periodicidad,infoInstrumentoElegido.tasaInteres)
-
-alert(`El monto final obtenido por la inversion seria de: ${montoFinalInvercion}`)
